c8899a10ce1fa42da19ecf0b8ae0bdf0ec3b2da2
smari-98 <m.manganese2@gmail.com>
2026-06-15 19:52:03 +0200

    first commit

---
+++ prove.py
@@ -0,0 +1,36 @@
+#syntax dictonary - A dictionary is a collection of unordered, modifiable(mutable) paired (key: value) data type.
+
+# syntax
+empty_dict = {}
+# Dictionary with data values
+#dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+#person = {
+   # 'first_name':'Maria',
+    #'last_name':'Rossi',
+    #'age':25,
+    #'country':'Italy',
+    #'is_marred':False,
+    #'skills':['JavaScript', 'C', 'Java', 'Python'],
+    #'address':{
+     #   'street':'Space street',
+      #  'zipcode':'00000'
+   # }
+    #}
+# print(person)
+#print()
+#num=int(input("inserisci un valore:"))
+
+#for a in range(1,num):
+ #   print(" - ", a)
+
+#stampa un triangolo formato da cancelletti
+for b in range (1, 10):
+    print(b*'#')
+
+
+for c in range(1,5):
+    
+    for d in range (1,5):
+        print(d*'*')
+
+    print(c*'#')
